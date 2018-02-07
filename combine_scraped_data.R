62ee0dcdd603ea9014e619eafa43cdd32a8b141c
bthomasbailey <bthomasbailey@gmail.com>
2018-02-06 23:44:16 -0500

    combine inspection and violation dataframes

--- combine_scraped_data.R
+++ combine_scraped_data.R
@@ -70,8 +70,11 @@ cle_restaurants <- cle_restaurants %>%
 
 cle_inspections <- read_csv("inspection_data_scraping/cleveland/data/inspections.csv")
 
+# Filter out Critical Control Point inspections, as these do not contain actual violations,
+# but rather codes that the restaurants are actually obeying
 cle_inspections <- cle_inspections %>% 
-                    mutate(inspect_date = dmy(cle_inspections$inspect_date))
+                    mutate(inspect_date = dmy(cle_inspections$inspect_date)) %>% 
+                    filter(inspect_type != "Critical Control Point")
 
 cle_violations <- read_csv("inspection_data_scraping/cleveland/data/violations.csv")
 
@@ -147,5 +150,79 @@ pitt_restaurants <- pitt_restaurants %>%
 
 restaurants <- bind_rows(to_restaurants, lv_restaurants, cu_restaurants, cle_restaurants, pitt_restaurants)
 
+
+# Combine Inspection DFs --------------------------------------------------
+# Prepend ID with city name in case IDs are same across cities
+names_inspection <- c("ID_Rest", "ID_Inspect", "Date")
+
+to_inspections <- to_inspections %>% 
+                      mutate(ID_Rest = str_c("to", establishment_id)) %>% 
+                      mutate(ID_Inspect = str_c("to", inspection_id)) %>% 
+                      rename(Date = inspection_date) %>% 
+                      select(names_inspection)
+
+lv_inspections <- lv_inspections %>% 
+                      mutate(ID_Rest = str_c("lv", Permit.Number)) %>% 
+                      mutate(ID_Inspect = str_c("lv", Serial.Number)) %>% 
+                      mutate(Date = ymd(Inspection.Date)) %>% 
+                      select(names_inspection)
+
+cu_inspections <- cu_inspections %>% 
+                      mutate(ID_Rest = str_c("cu", id_facility)) %>% 
+                      mutate(ID_Inspect = str_c("cu", id_inspection)) %>% 
+                      rename(Date = date) %>% 
+                      select(names_inspection)
+
+cle_inspections <- cle_inspections %>% 
+                      mutate(ID_Rest = str_c("cle", id_restaurant)) %>% 
+                      mutate(ID_Inspect = str_c("cle", id_inspection)) %>% 
+                      rename(Date = inspect_date) %>% 
+                      select(names_inspection)
+
+pitt_inspections <- pitt_inspections %>% 
+                      mutate(ID_Rest = str_c("pitt", client_id)) %>% 
+                      mutate(ID_Inspect = str_c("pitt", id_inspection)) %>% 
+                      rename(Date = dt_inspection) %>% 
+                      select(names_inspection)
+
+inspections <- bind_rows(to_inspections, lv_inspections, cu_inspections, cle_inspections, pitt_inspections)
+
+
+# Combine Violation DFs --------------------------------------------------
+names_violation <- c("ID_Inspect", "Description")
+
+to_violations <- to_violations %>% 
+                    mutate(ID_Inspect = str_c("to", inspection_id)) %>% 
+                    rename(Description = infraction_details) %>% 
+                    select(names_violation)
+  
+lv_violations <-  lv_violations %>% 
+                    mutate(ID_Inspect = str_c("lv", Serial.Number)) %>% 
+                    rename(Description = Violation.Description) %>% 
+                    select(names_violation)
+  
+cu_violations <- cu_violations %>% 
+                    mutate(ID_Inspect = str_c("cu", id_inspection)) %>% 
+                    filter(in_compliance == "OUT") %>% 
+                    rename(Description = description) %>% 
+                    select(names_violation)
+  
+cle_violations <- cle_violations %>% 
+                    mutate(ID_Inspect = str_c("cle", id_inspection)) %>%      
+                    rename(Description = observed_violation) %>% 
+                    select(names_violation)
+
+pitt_violations <- pitt_violations %>% 
+                    mutate(ID_Inspect = str_c("pitt", id_inspection)) %>% 
+                    rename(Description = desc_violation) %>% 
+                    select(names_violation)
+
+violations <- bind_rows(to_violations, lv_violations, cu_violations, cle_violations, pitt_violations)
+
+
+
+# Save restaurants, inspections, & violations DFs as CSV files ------------
 if(!dir.exists("data")) {dir.create("data")}
 write_csv(restaurants, "data/restaurants.csv")
+write_csv(inspections, "data/inspections.csv")
+write_csv(violations, "data/violations.csv")
